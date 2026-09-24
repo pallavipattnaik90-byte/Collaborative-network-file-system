c89710764d1d173e7cf9099b072f41f3eb2f31db
Amrit07-ML <amritrajmukund8877@gmail.com>
2026-09-24 19:15:50 +0530

    Enable multi-client handling in server
    Refactor server to handle multiple clients using threads.

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 5000
 
+def handle_client(conn, addr):
+    print("Client connected:", addr)
+
+    conn.sendall(b"Connected to Collaborative File Server")
+
+    conn.close()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server.bind((HOST, PORT))
-server.listen(1)
+server.listen()
 
 print("Server started...")
-print("Waiting for a client...")
+print("Waiting for clients...")
 
-conn, addr = server.accept()
+while True:
+    conn, addr = server.accept()
 
-print("Client connected:", addr)
+    thread = threading.Thread(
+        target=handle_client,
+        args=(conn, addr)
+    )
 
-conn.sendall(b"Connected to Collaborative File Server")
-
-conn.close()
-server.close()
+    thread.start()
